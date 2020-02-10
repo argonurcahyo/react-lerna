359480e86fee1d3e6d4a1c5b47b983cd19b226b0
Yoga Derana <yderana@gmail.com>
2020-02-10 21:01:45 +0700

    update interface

--- packages/sample-app/src/interfaces/iresponse-customer.tsx
+++ packages/sample-app/src/interfaces/iresponse-customer.tsx
@@ -1,11 +1,10 @@
 export interface IResponseCustomer {
-    "id": "1",
-    "username": "anitarachma",
-    "email": "anitarachma@gmail.com",
-    "fullname": "Anita Rachma",
-    "tipe_akun": "Eksternal",
-    "pengajuan": "Menunggu Persetujuan",
-    "moda": "Eksternal",
-    "tgl_daftar": "2019-07-21",
-    "version": 1
+    id: string,
+    username?: string,
+    email?: string,
+    fullname?: string,
+    tipe_akun?: string,
+    pengajuan?: string,
+    moda?: string,
+    tgl_daftar?: string,
 }
